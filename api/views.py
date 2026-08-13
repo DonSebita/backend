27276795c4ee22c95f57cafb9cfb9177cb317d8c
DonSebita <luxioxbox49@gmail.com>
2026-08-13 13:39:15 -0400

    inicializacion de django

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# from django.shortcuts import render 
+
+from rest_framework import viewsets
+from .serializer import ProgrammerSerializer
+from .models import programmer
+
+class ProgrammerViewSet(viewsets.ModelViewSet):
+    queryset = programmer.objects.all()
+    serializer_class = ProgrammerSerializer
